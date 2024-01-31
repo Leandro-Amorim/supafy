c37935234b04c3cdba2fc2c8c6aaa3e07ce388db
Leandro Amorim <leandroamorimdev@gmail.com>
2024-01-31 15:59:59 -0300

    Redirect to complete profile

--- src/components/shared/modals/LoginModal.tsx
+++ src/components/shared/modals/LoginModal.tsx
@@ -1,15 +1,28 @@
 import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react"
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
-import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton, useDisclosure} from '@chakra-ui/react'
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton, useDisclosure } from '@chakra-ui/react'
 import LoginButton from "../buttons/LoginButton";
 import { Database } from "@/supabase";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function LoginModal() {
 	const supabaseClient = useSupabaseClient<Database>();
 	const user = useUser();
+	const router = useRouter();
 
 	const { isOpen, onOpen, onClose } = useDisclosure();
+
+	useEffect(() => {
+		const { data: { subscription } } = supabaseClient.auth.onAuthStateChange((event) => {
+			if (event === "SIGNED_IN") {
+				router.push('/complete_profile');
+			}
+		});
+		return () => subscription.unsubscribe();
+	});
+
 
 	if (!user)
 		return (
